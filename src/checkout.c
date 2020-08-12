07396892074d7d0e3bb88c1449bed83e67ab9fec
Tyler Ang-Wanek <tylerw@axosoft.com>
2020-08-12 14:30:00 -0700

    thread checkout: stub indirection for threading

--- src/checkout.c
+++ src/checkout.c
@@ -1944,7 +1944,7 @@ static int checkout_create_the_new_perform(
 	return 0;
 }
 
-static int checkout_create_the_new(
+static int checkout_create_the_new__single(
 	unsigned int *actions,
 	checkout_data *data)
 {
@@ -1965,6 +1965,15 @@ static int checkout_create_the_new(
 	}
 
 	return 0;
+}
+
+static int checkout_create_the_new(
+	unsigned int *actions,
+	checkout_data *data)
+{
+#ifdef GIT_THREADS
+#endif
+	return checkout_create_the_new__single(actions, data);
 }
 
 static int checkout_create_submodules(
